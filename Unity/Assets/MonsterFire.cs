22cc216053deee0c59ca9a334894a3d0ebf6461d
YuIshikawa <s1320793@u.tsukuba.ac.jp>
2016-10-29 19:11:16 +0900

    一定数（200個）以上のオブジェクトを削除🤑

--- Unity/Assets/MonsterFire.cs
+++ Unity/Assets/MonsterFire.cs
@@ -7,8 +7,8 @@ public class MonsterFire : MonoBehaviour {
 
     public string str;
     private int modelNum;
-    private List<Transform> prefabs = new List<Transform>();
-
+    private List<GameObject> prefabs = new List<GameObject>();
+    private const int maxObjectNum = 200;
 
     // Use this for initialization
     void Start () {
@@ -50,13 +50,22 @@ public class MonsterFire : MonoBehaviour {
         GameObject temp = (GameObject)Resources.Load(pos);
         if (temp != null)
         {
-            prefabs.Add(temp.transform);  //正しく動く？
             num = prefabs.Count;//リストが削除されることを考えていない
             //Instantiate(prefabs[num - 1], new Vector3(0f, 1f, 0f), Quaternion.identity);
             temp = PhotonNetwork.Instantiate(pos, transform.position + new Vector3(0.0f, 1.0f, 0.0f) + (transform.forward * 0.5f), transform.rotation, 0);
+            prefabs.Add(temp);
             temp.GetComponent<Rigidbody>().velocity = transform.forward * 15.0f;
-
+            checkObjectNum(num);
         }
     }
 
+
+    void checkObjectNum(int num)
+    {
+        if(maxObjectNum <= num)
+        {
+            PhotonNetwork.Destroy(prefabs[0]);
+            prefabs.RemoveAt(0);
+        }
+    }
 }
