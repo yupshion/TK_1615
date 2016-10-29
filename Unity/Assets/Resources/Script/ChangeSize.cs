a5b72d12e5e21589b2a09731398c1f7ee099b9e9
yupshion <volcarona9227@gmail.com>
2016-10-29 19:27:05 +0900

    Resourceの中を変更👻

---
+++ Unity/Assets/Resources/Script/ChangeSize.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSize : MonoBehaviour {
+
+	public Vector3 size;
+	public Vector3 max;
+	public Vector3 min;
+	public bool isChangingSize = false;
+
+	// Use this for initialization
+	void Start () {
+
+		size = new Vector3(2, 2, 2);
+		this.transform.localScale = size;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (isChangingSize) {
+			this.transform.localScale = size;
+			Debug.Log ("ChangeSize");
+		}
+	}
+}
---
+++ Unity/Assets/Resources/Script/FaceToMainCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceToMainCamera : MonoBehaviour {
+
+	/// <summary>
+	/// ビルボード処理の対象となるカメラを指定します。
+	/// </summary>
+	public Camera targetCamera;
+
+	/// <summary>
+	// Use this for initialization
+	/// </summary>
+	void Start ()
+	{
+		//対象のカメラが指定されない場合にはMainCameraを対象とします。
+		if (this.targetCamera == null)
+			targetCamera = Camera.main;
+	}
+
+	/// <summary>
+	// Update is called once per frame
+	/// </summary>
+	void Update ()
+	{
+		//カメラの方向を向くようにする。
+		this.transform.LookAt(this.targetCamera.transform.position);        
+	}
+}
