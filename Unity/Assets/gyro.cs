d8031af8a154ebee97b8a75fed956225a866447b
butasimeji <ueda.yuki@entcomp.esys.tsukuba.ac.jp>
2016-10-30 09:42:36 +0900

    🍜〜
    2種類のVRモードがはいってます

---
+++ Unity/Assets/gyro.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gyro : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Input.gyro.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.AngleAxis(90.0f,Vector3.right)*Input.gyro.attitude*Quaternion.AngleAxis(180.0f,Vector3.forward);
+	}
+}
