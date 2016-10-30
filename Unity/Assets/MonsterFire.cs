d2605b6071aa9c3b7837817ad1b3d81d5622415d
YuIshikawa <s1320793@u.tsukuba.ac.jp>
2016-10-30 09:55:49 +0900

    implemented the way to indicate the cue depending on voice. And then, the size of objects are changed, too. However, modification is needed.

--- Unity/Assets/MonsterFire.cs
+++ Unity/Assets/MonsterFire.cs
@@ -22,6 +22,43 @@ public class MonsterFire : MonoBehaviour {
 			GameObject bullet = PhotonNetwork.Instantiate ("Bullet", transform.position + new Vector3 (0.0f, 1.0f, 0.0f) + (transform.forward * 0.5f), transform.rotation, 0);
 			bullet.GetComponent<Rigidbody> ().velocity = transform.forward * 15.0f;
 		}
+/*
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            chooseModelInputText("apple_10");
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            chooseModelInputText("apple_50");
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            chooseModelInputText("apple_100");
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            chooseModelInputText("gorilla_10");
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            chooseModelInputText("gorilla_50");
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            chooseModelInputText("gorilla_100");
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            chooseModelInputText("swiftstart");
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            chooseModelInputText("swiftstop");
+        }
+*/
+
     }
 
     void chooseModelInputText(string message)
@@ -33,20 +70,27 @@ public class MonsterFire : MonoBehaviour {
 
 		int num = 0;
 
+        if (message.IndexOf("_")<=0) {
+            if (message == "swiftstart")
+            {
+                GameObject.Find("RawImage").GetComponent<RawImage>().enabled = true;
+            }
+
+            if (message == "swiftstop")
+            {
+                GameObject.Find("RawImage").GetComponent<RawImage>().enabled = false;
+            }
+            return;
+        }
+
+        //string[0]:word, string[1]:value for scale
+        string[] stArrayData = message.Split('_');
+        int objScale = int.Parse(stArrayData[1]);
+
         string pos = "Prefab/";
-        pos += message;
+        pos += stArrayData[0];
 
-
-        if (message == "swiftstart")
-        {
-            GameObject.Find("RawImage").GetComponent<RawImage>().enabled = true;
-        }
-
-        if (message == "switftstop")
-        {
-            GameObject.Find("RawImage").GetComponent<RawImage>().enabled = false;
-        }
-
+        Debug.Log(stArrayData[0]);
 
         //GameObject temp = (GameObject)Resources.Load(modelName);
         GameObject temp = (GameObject)Resources.Load(pos);
@@ -56,8 +100,8 @@ public class MonsterFire : MonoBehaviour {
             num = prefabs.Count;//リストが削除されることを考えていない
             //Instantiate(prefabs[num - 1], new Vector3(0f, 1f, 0f), Quaternion.identity);
             temp = PhotonNetwork.Instantiate(pos, transform.position + new Vector3(0.0f, 1.0f, 0.0f) + (transform.forward * 0.5f), transform.rotation, 0);
-//			particle = Instantiate (particle, transform.position + new Vector3(0.0f, 1.0f, 0.0f) + (transform.forward * 0.5f), transform.rotation) as GameObject;
-
+            //			particle = Instantiate (particle, transform.position + new Vector3(0.0f, 1.0f, 0.0f) + (transform.forward * 0.5f), transform.rotation) as GameObject;
+            temp.transform.localScale = new Vector3((float)((float)objScale/100.0), (float)((float)objScale / 100.0), (float)((float)objScale / 100.0));
             temp.GetComponent<Rigidbody>().velocity = transform.forward * 15.0f;
 //            checkObjectNum(num);
         }
